96fd23c1d2d4cead162a619f363c9e6678640a5d
Daniel Diaz <diazlunadaniel24@gmail.com>
2024-07-19 21:22:12 -0600

    brindar informacion
    depende de ciertas palabras te brindara una informacion sobre esa palabra

---
+++ buscar_informacion/src/buscar_informacion/Buscar_informacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package buscar_informacion;
+
+/**
+ *
+ * @author ABC
+ */
+public class Buscar_informacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
