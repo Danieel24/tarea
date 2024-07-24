424e6db1ecf973710d3bf8d164845fd129d4c937
Daniel Diaz <diazlunadaniel24@gmail.com>
2024-07-24 16:42:41 -0600

    funciones 1
    ingresas un numero entero y el programa te dice si es primo o no

---
+++ funciones1/src/funciones1/Funciones1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funciones1;
+
+/**
+ *
+ * @author ABC
+ */
+import java.util.*;
+public class Funciones1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+
+        if (primo(numero)) {
+            System.out.println("El número " + numero + " es primo.");
+        } else {
+            System.out.println("El número " + numero + " no es primo.");
+        }
+    }
+
+    public static boolean primo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
